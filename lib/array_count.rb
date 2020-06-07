acdf2339a0bc714d6061fdce5519f9d7a840ed16
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-07 12:54:06 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,10 +2,12 @@ def count_strings(array)
   array.count do |element|
     element.class == String
   end
+  count_strings
 end
 
 def count_empty_strings(array)
   array.count do |element|
     element == "" # alternative solution: element.class == String && element.empty?
   end
+  count_empty_strings
 end
